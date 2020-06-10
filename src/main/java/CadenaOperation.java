e23dce0c19b855b5cfc08ac8bc1fd1f9746de50e
unknown <cctc2013@gmail.com>
2020-06-10 17:56:25 -0400

    Comentarios mas verificacion de error

--- src/main/java/CadenaOperation.java
+++ src/main/java/CadenaOperation.java
@@ -4,12 +4,24 @@
  *
  */
 public class CadenaOperation {
+	/**
+	 * Metodo para concatenar dos cadenas
+	 * @param st1 cadena 1
+	 * @param st2 cadena dos
+	 * @return la cadena concatenada
+	 */
 	public static String concatDosCadena(String st1, String st2) {
 		if(st1.equals(null) || st2.equals(null)) {
 			throw new NullPointerException("No se aceptan valores nulos");
 		}
 		return st1+st2;
 	}
+	/**
+	 * Sirve para concatenar maws de un String
+	 * @param <T> Generics que hereda de String
+	 * @param args Para recibir multiple String
+	 * @return la concatenacion de las cadenas
+	 */
 	public static <T extends String> String concatMultiple(T...  args) {
 		String cad = "";
 		for(String s: args) {
